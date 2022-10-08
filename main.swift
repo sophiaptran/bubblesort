3dd3b4b64e1ab401017f69b3b3da1975ef228bab
sophia-tran <Sophia Tran>
2022-10-08 18:08:44 -0500

    bubble ordered 0

--- main.swift
+++ main.swift
@@ -1,35 +1,34 @@
+var words = [String]()
+var line = readLine()
+while line != nil && line != "" && line != " " {
+    words.append(line!)
+    line = readLine()
+}
+var  tempArray = words
 
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let unsortedStrings = ["paper", "tree", "apple", "book", "sun"]
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
-
-// Add your code below:
-
-func swap(strings: inout [String], firstIndex: Int, secondIndex: Int) {
-    let temp = strings[firstIndex]
-    strings[firstIndex] = strings[secondIndex]
-    strings[secondIndex] = temp
+func swap(words: inout [String], firstIndex: Int, secondIndex: Int) {
+    let temp = words[firstIndex]
+    words[firstIndex] = words[secondIndex]
+    words[secondIndex] = temp
 }
 
 func sort() {
     var tempCount = 0
     var totalCount = 0
     var functIn = true
-    var unsort = unsortedStrings
-    print("Pass: \(0), Swaps: \(tempCount)/\(totalCount), Array: \(unsort)")
+    var unsort = words
+    print("Pass: \(0), Swaps: \(tempCount)/\(totalCount), Array: \(words)")
     while(functIn) {
-        for i in 0..<unsort.count {
-            for k in 1 ..< unsort.count - i {
-                if (unsort[k]<unsort[k-1]){
+        for i in 0..<words.count {
+            for k in 1 ..< words.count - i {
+                if (words[k]<words[k-1]){
                     functIn = true
                     tempCount += 1
                     totalCount += 1
-                    swap(strings:&unsort, firstIndex: k, secondIndex: k - 1)
+                    swap(words:&words, firstIndex: k, secondIndex: k - 1)
                 }
             }
-            print("Pass: \(i + 1), Swaps: \(tempCount)/\(totalCount), Array: \(unsort)")
+            print("Pass: \(i + 1), Swaps: \(tempCount)/\(totalCount), Array: \(words)")
             if(tempCount == 0){
                 functIn = false
                 break
